3e6a677f31f6dddb90a74ebbdb49bb0192b3bea9
Kyle Harrity <hpkyh47@gmail.com>
2026-04-15 11:52:18 -0400

    fix pawn promotion bug

--- src/board-state.ts
+++ src/board-state.ts
@@ -264,9 +264,9 @@ export class BoardStateMachine {
     }
     if (
       fromPiece instanceof Pawn &&
-      (Position.fromString(to).isBeginningOfColumn() ||
-        Position.fromString(to).isEndOfColumn())
-    ) {
+      ( (fromPiece.color === "black" && Position.fromString(to).isBeginningOfColumn()) ||
+        (fromPiece.color === "white" && Position.fromString(to).isEndOfColumn()) )
+      ) {
       return {
         state: {
           ...state,
